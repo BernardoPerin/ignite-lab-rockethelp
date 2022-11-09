e59e33e3666093ac5a12459f0fdb13cb0854d3f1
Bernardo Perin <bernardo_perin@hotmail.com>
2022-11-09 01:49:54 -0300

    Estrutura visual e Navegação

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList } from 'native-base';
-import { SignOut } from 'phosphor-react-native';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
+import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order'; 
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open'); 
-  const [orders, setOrders] = useState<OrderProps[]>([{
-    id: '123',
-    patrimony: '123456',
-    when: '18/072022 às 10:00',
-    status: 'open'
-  }]);
+  const [orders, setOrders] = useState<OrderProps[]>([
+   // {
+    //  id: '123',
+    //  patrimony: '123456',
+    //  when: '18/072022 às 10:00',
+    //  status: 'open'
+   // }
+  ]);
 
   const { colors } = useTheme();
   
@@ -67,7 +70,20 @@ export function Home() {
           data={orders}
           keyExtractor={item => item.id}
           renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+                <ChatTeardropText color={colors.gray[300]} size={40} />
+                <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                  Você ainda não possui {'\n'}
+                  solicitações
+                </Text>
+            </Center>
+          )}
         />
+
+        <Button title="Nova solicitação" />
       </VStack>
     </VStack>
   );
